5798decbdacbb8afd3d9a4772930b63094fded08
Aditya U <im.adityau@gmail.com>
2022-08-19 03:04:31 -0700

    add sql_guid to erlang otp odbc

---
+++ erts/emulator/x86_64-unknown-linux-gnu/erl_version.h
@@ -0,0 +1,10 @@
+/* This file was created by 'make_version' -- don't modify. */
+#define ERLANG_OTP_RELEASE "22"
+#define ERLANG_OTP_VERSION "22.0"
+#define ERLANG_VERSION "10.4"
+#if ERTS_SAVED_COMPILE_TIME
+#  define ERLANG_COMPILE_DATE "Fri Aug 19 02:15:55 2022"
+#else
+#  define ERLANG_COMPILE_DATE ""
+#endif
+#define ERLANG_ARCHITECTURE "x86_64-unknown-linux-gnu"
